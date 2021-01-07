550bef5dfe217553d01ac7f4954dd38ec86c02cf
ahtchow <ahtchow@gmail.com>
2021-01-07 04:58:16 -0500

    testing with bipedal walker

--- TD3_agent.py
+++ TD3_agent.py
@@ -2,6 +2,7 @@
 import numpy as np
 import random
 from collections import namedtuple, deque
+from ornstien_uhlenbeck_noise import OUNoise
 import copy
 
 # Deep Learning Imports
@@ -20,10 +21,7 @@ TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 0.0001       # learning rate of the actor
 LR_CRITIC = 0.001       # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
-ACTION_NOISE_INIT = 0.3 # Initial Controls for Action Exploration
-ACTION_NOISE_MIN = 0.1  # Initial Controls for Action Exploration
-NOISE_DECAY = 0.95      # Decay for Action Exploration
-NOISE_CLIP = 0.5
+NOISE_CLIP = 0.5        # Max Noise
 UPDATE_EVERY = 2        # LEARN EVERY N STEPS
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -45,7 +43,8 @@ class TD3:
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(random_seed)
-        self.exploration_noise = ACTION_NOISE_INIT
+        # Noise used for exploration, added to action
+        self.noise = OUNoise(action_size, random_seed) # Noise process
 
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed, device)
@@ -67,20 +66,21 @@ class TD3:
         self.critic_target_2 = Critic(state_size, action_size, random_seed).to(device)
         # Recall we will only update the local network, then perform soft update on target
         self.critic_optimizer_2 = optim.Adam(self.critic_2.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+        
 
-    def act(self, state):
+    def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
         state = torch.FloatTensor(state.reshape(1, -1)).to(device)
-        action = self.actor(state).cpu().data.numpy().flatten()
+        action = self.actor_local(state).cpu().data.numpy().flatten()
 
         # Add some noise for exploration
-        action += np.random.normal(0, self.exploration_noise, size=1.0)
-        self.exploration_noise = np.max(ACTION_NOISE_MIN, self.exploration_noise * NOISE_DECAY)
+        if add_noise:
+            action += self.noise.sample()
         action = action.clip(-1, 1)
         return action
 
 
-    def step(self, state, action, reward, next_state, done):
+    def step(self, state, action, reward, next_state, done, t_step):
         """Save experience in replay memory, and use random sample from buffer to learn."""
         # Save experience / reward
         self.memory.add(state, action, reward, next_state, done)
@@ -88,10 +88,10 @@ class TD3:
         # Learn, if enough samples are available in memory
         if len(self.memory) > BATCH_SIZE:
             experiences = self.memory.sample()
-            self.learn(experiences, GAMMA)
+            self.learn(experiences, GAMMA, t_step)
 
 
-    def learn(self, experiences, gamma, step_num):
+    def learn(self, experiences, gamma, t_step):
         """Update policy and value parameters using given batch of experience tuples.
         Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
         where:
@@ -108,16 +108,16 @@ class TD3:
 
         # Select next action according to target policy:
         actions_cpy = copy.copy(actions)
-        noise = actions_cpy.data.normal_(mean=0, std=0.2).to(device)
+        noise = self.noise.sample()
         noise = noise.clamp(-NOISE_CLIP, NOISE_CLIP)
         next_actions = (self.actor_target(next_states) + noise)
-        next_actions = next_action.clamp(-1, 1)
+        next_actions = next_actions.clamp(-1, 1)
 
         # Compute target Q-value:
         target_Q1 = self.critic_target_1(next_states, next_actions)
         target_Q2 = self.critic_target_2(next_states, next_actions)
         target_Q = torch.min(target_Q1, target_Q2)
-        target_Q = reward + ((1-dones) * gamma * target_Q).detach()
+        target_Q = rewards + ((1-dones) * gamma * target_Q).detach()
 
         # Optimize Critic 1:
         current_Q1 = self.critic_1(states, actions)
@@ -127,17 +127,17 @@ class TD3:
         self.critic_optimizer_1.step()
 
         # Optimize Critic 2:
-        current_Q1 = self.critic_2(states, actions)
+        current_Q2 = self.critic_2(states, actions)
         loss_Q2 = F.mse_loss(current_Q2, target_Q)
         self.critic_optimizer_2.zero_grad()
         loss_Q2.backward()
         self.critic_optimizer_2.step()
 
         # Delayed policy updates:
-        if step_num % UPDATE_EVERY == 0:
+        if t_step % UPDATE_EVERY == 0:
             # Compute actor loss:
             policy_actions = self.actor_local(states)
-            actor_loss = -self.critic_1(state_sizes, policy_actions).mean()
+            actor_loss = -self.critic_1(states, policy_actions).mean()
 
             # Optimize the actor
             self.actor_optimizer.zero_grad()
@@ -145,8 +145,8 @@ class TD3:
             self.actor_optimizer.step()
 
             # Soft Updates
-            self.soft_update(self.critic_local_1, self.critic_target_1, TAU)
-            self.soft_update(self.critic_local_2, self.critic_local_2, TAU)
+            self.soft_update(self.critic_1, self.critic_target_1, TAU)
+            self.soft_update(self.critic_2, self.critic_target_2, TAU)
             self.soft_update(self.actor_local, self.actor_target, TAU)
 
 
@@ -162,3 +162,7 @@ class TD3:
 
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau * local_param.data  + (1.0 - tau) * target_param.data)
+    
+    
+    def reset(self):
+        self.noise.reset()
